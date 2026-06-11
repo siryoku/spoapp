6cd21165c8c51f7716777c7d133d6779102e32c4
shibatasuguru0311 <suguru.shibata@blowish.jp>
2026-06-11 16:16:08 +0900

    practice pytest

--- src/code.py
+++ src/code.py
@@ -1,20 +1,30 @@
-import requests
+# import requests
 
-def sum_numbers(a, b):
-  return (a + b), a
+# def sum_numbers(a, b):
+#   return (a + b), a
 
-def get_json_data(id):
-  res = requests.get('http://xxxx', param={'id':id})
-  res_json = res.json()
-  return res_json
+# def get_json_data(id):
+#   res = requests.get('http://xxxx', param={'id':id})
+#   res_json = res.json()
+#   return res_json
 
-def get_user_names(user_ids):
-  user_names = {}
-  for id in user_ids:
-    json_data = get_json_data(id)
-    user_names[id] = json_data['name']
-  return user_names
+# def get_user_names(user_ids):
+#   user_names = {}
+#   for id in user_ids:
+#     json_data = get_json_data(id)
+#     user_names[id] = json_data['name']
+#   return user_names
 
-def user_name_validation(user_name):
-  if user_name == None:
-    raise ValueError('名前が設定されていません')
+# def user_name_validation(user_name):
+#   if user_name == None:
+#     raise ValueError('名前が設定されていません')
+
+
+# def add(a, b):
+  # return a + b
+
+def is_adult(age):
+  return age >= 18
+
+def divide(a, b):
+  return a / b
--- tests/test_code.py
+++ tests/test_code.py
@@ -1,23 +1,54 @@
+# import pytest
+# from src import code
+
+# def test_sum_numbers():
+#   result1, result2 = code.sum_numbers(1, 2)
+#   assert result1 == 3
+#   assert result2 == 1
+
+# def get_json_data_mock(id):
+#   return {'name':'サプー'}
+
+# def test_get_user_names(monkeypatch):
+#   monkeypatch.setattr(
+#     code, 'get_json_data', get_json_data_mock)
+#   result = code.get_user_names(['001', '009'])
+
+#   assert list(result.keys()) == ['001', '009']
+#   assert list(result.values()) == ['サプー', 'サプー']
+
+# def test_user_name_validation():
+#   with pytest.raises(ValueError) as e:
+#     code.user_name_validation(None)
+#   assert str(e.value) == '名前が設定されていません'
+
+# from code import add
+
+# def test_add():
+#   assert　add(1, 2) == 3
+
 import pytest
-from src import code
+from src.code import is_adult, divide
 
-def test_sum_numbers():
-  result1, result2 = code.sum_numbers(1, 2)
-  assert result1 == 3
-  assert result2 == 1
+def test_is_adult():
+  assert is_adult(20)
+  assert is_adult(18)
+  assert not is_adult(17)
 
-def get_json_data_mock(id):
-  return {'name':'サプー'}
+def test_divide():
+  with pytest.raises(ZeroDivisionError):
+    divide(10, 0)
 
-def test_get_user_names(monkeypatch):
-  monkeypatch.setattr(
-    code, 'get_json_data', get_json_data_mock)
-  result = code.get_user_names(['001', '009'])
+def mock_get_json_data(user_id):
+  return {"name": "サプー"}
 
-  assert list(result.keys()) == ['001', '009']
-  assert list(result.values()) == ['サプー', 'サプー']
+def test_get_user_name(monkeypatch):
+  monkeypatch.assert(
+    code,
+    "get_json_data",
+    mock_get_json_data
+  )
+  result = code.get_user_name("001")
+  assert result == "サプー"
 
-def test_user_name_validation():
-  with pytest.raises(ValueError) as e:
-    code.user_name_validation(None)
-  assert str(e.value) == '名前が設定されていません'
+
